c0c82a7089289d6ac4b7a8370e9b21e253185498
RemxFr <epourexisterpro@gmail.com>
2023-11-23 17:08:11 +0100

    initial commit

---
+++ src/main/java/com/fr/inscriptionrabbitmq/InscriptionRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.fr.inscriptionrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InscriptionRabbitMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InscriptionRabbitMqApplication.class, args);
+    }
+
+}
